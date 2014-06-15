114e5ba56590089f862ac09fa27d8693b75681c1
Doug Ayers <douglascayers@gmail.com>
2014-06-15 18:26:55 -0500

    section two challenges

--- webapp/app.js
+++ webapp/app.js
@@ -1,19 +1,30 @@
 (function(){
 
-	var app = angular.module( 'store', [ 'ui.boostrap' ] );
+	var app = angular.module( 'store', [ 'ui.bootstrap' ] );
 
 	app.controller( 'StoreController', function() {
 
 		// define a property of this controller
-		this.product = gem;
+		this.products = gems;
 
 	});
 
 	// test data, normally make a REST call
-	var gem = {
-		name: 'Dodecahedron',
-		price: 2.95,
-		description: '12 sided gem'
-	};
+	var gems = [
+		{
+			name: 'Dodecahedron',
+			price: 2,
+			description: '12 sided gem',
+			canPurchase: true,
+			soldOut: true
+		},
+		{
+			name: 'Pentagonal Gem',
+			price: 5.95,
+			description: '5 sided gem',
+			canPurchase: false,
+			soldOut: false
+		}
+	];
 
 })();
